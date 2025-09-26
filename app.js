8b8c254faf24905267da3bb69405a704f7786611
Tikileo <christpher.esguerra@gmail.com>
2025-09-26 14:07:35 -0400

    Reupdate files to work ver.01

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 // app.js Christopher Esguerra 301483615 September 24, 2025
-<<<<<<< HEAD
-var serverless = require('serverless-http');
-=======
->>>>>>> 3f92b8d0bdb464736bd234c921e5bd5dd85632e0
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 3f92b8d0bdb464736bd234c921e5bd5dd85632e0
 var indexRouter = require('./routes/index');
 var aboutRouter = require('./routes/about');
 var contactRouter = require('./routes/contact');
@@ -21,15 +12,22 @@ var serviceRouter = require('./routes/service');
 
 var app = express();
 
+// Determine the root directory (handle both local and serverless environments)
+var rootDir = __dirname;
+if (__dirname.includes('.netlify/functions-serve')) {
+  // When running in Netlify functions, adjust the path
+  rootDir = path.resolve(__dirname, '../../../..');
+}
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(rootDir, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
